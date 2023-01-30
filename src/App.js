ede70bf486511ae8ade5ae2b8e19892a9d64cee5
BCN MAC 021 <bcnmac021@MacBook-Pro-de-BCN.local>
2023-01-30 10:49:58 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+
+import { Route, BrowserRouter, Routes } from 'react-router-dom';
+import ShowElement from './components/ShowElement';
+import  Create  from './components/Create';
+import Edit from './components/Edit'
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+          <Routes>
+            <Route path='/' element={<ShowElement />} />
+            <Route path='/create' element={<Create/>} />
+            <Route path='/edit/:id' element={<Edit/>}/>
+          </Routes>
+      </BrowserRouter>
+     
     </div>
   );
 }
